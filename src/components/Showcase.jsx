892f5358a3e112828c73ade874872a414ab327d0
Oluwatobi <oluwatobioreniyi@gmail.com>
2026-07-28 22:56:27 +0100

    Remove redundant gsap register

--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -3,8 +3,6 @@ import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useEffect, useRef } from "react";
 
-gsap.registerPlugin(ScrollTrigger);
-
 const Showcase = () => {
   const videoRef = useRef(null);
 
